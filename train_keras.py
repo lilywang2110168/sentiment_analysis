18b46e4b6db6bad0e95e274c3b7aaa9f8f4fbd95
wangz20 <zhe.wang@vanderbilt.edu>
2018-04-24 13:31:10 -0500

    message

--- train_keras.py
+++ train_keras.py
@@ -58,5 +58,5 @@ print(model.summary())
 model.fit(X_train, y_train, epochs=1, callbacks=[tensorBoardCallback], batch_size=64)
 
 # Evaluation on the test set
-#scores = model.evaluate(X_test, y_test, verbose=0)
-#print("Accuracy: %.2f%%" % (scores[1]*100))
+scores = model.evaluate(X_test, y_test, verbose=0)
+print("Accuracy: %.2f%%" % (scores[1]*100))
