5b66adc0ac870bd99128d6e1f5070e2dd15d35cd
wangz20 <zhe.wang@vanderbilt.edu>
2018-04-24 13:06:43 -0500

    update

---
+++ train_keras.py
@@ -0,0 +1,61 @@
+# LSTM for sequence classification in the IMDB dataset
+import numpy
+from keras.datasets import imdb
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM, Convolution1D, Flatten, Dropout
+from keras.layers.embeddings import Embedding
+from keras.preprocessing import sequence
+from keras.callbacks import TensorBoard
+
+# Using keras to load the dataset with the top_words
+top_words = 10000
+(X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=top_words, skip_top=10)
+
+# Pad the sequence to the same length...preceding zeros
+max_review_length = 1600
+
+# each review has been preoprocessed and uses frequency as the number.
+X_train = sequence.pad_sequences(X_train, maxlen=max_review_length)
+X_test = sequence.pad_sequences(X_test, maxlen=max_review_length)
+
+
+
+# Using embedding from Keras
+embedding_vecor_length = 300
+# this is the output length?? hmmm
+model = Sequential()
+model.add(Embedding(top_words, embedding_vecor_length, input_length=max_review_length))
+## urns positive integers (indexes) into dense vectors of fixed size. eg. [[4], [20]] -> [[0.25, 0.1], [0.6, -0.2]]
+# How come CNN can still be used???????? ugh...why...
+## try a pretrained model too???
+# use Google's pretrained word to vec??
+
+
+'''
+# Convolutional model (3x conv, flatten, 2x dense)
+model.add(Convolution1D(64, 3, padding='same'))
+model.add(Convolution1D(32, 3, padding='same'))
+model.add(Convolution1D(16, 3, padding='same'))
+model.add(Flatten())
+model.add(Dropout(0.2))
+model.add(Dense(180,activation='sigmoid'))
+model.add(Dropout(0.2))
+model.add(Dense(1,activation='sigmoid'))
+'''
+
+# now it's RNN
+
+model.add(LSTM(32, dropout=0.2, recurrent_dropout=0.2))
+model.add(Dense(1,activation='sigmoid'))
+
+
+# Log to tensorboard
+tensorBoardCallback = TensorBoard(log_dir='./logs', write_graph=True)
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+model.fit(X_train, y_train, epochs=1, callbacks=[tensorBoardCallback], batch_size=64)
+
+# Evaluation on the test set
+#scores = model.evaluate(X_test, y_test, verbose=0)
+#print("Accuracy: %.2f%%" % (scores[1]*100))
